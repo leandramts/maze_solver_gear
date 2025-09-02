3f2c6ac38fc133969b68db7e0774e4d0fe1ff0f0
Leandra Matos <leandra.mts10@gmail.com>
2025-09-02 12:38:40 -0400

    movimento de mão direita e ajuste nos motores

--- maze_solver.ino
+++ maze_solver.ino
@@ -6,10 +6,18 @@
 #define ECHO_E
 #define TRIG_D
 #define ECHO_D
-#define MOTOR_E
-#define MOTOR_D
+
+//motor esquerdo
+#define in1  
+#define in2 
+
+//motor direito
+#define in3 
+#define in4 
 
 #define VELOCIDADE_SOM_MS 343
+
+#define DIST_P 5 //distancia pra detectar parede
 
 void setup()
 {
@@ -24,14 +32,19 @@ void setup()
   digitalWrite(TRIG_E, LOW);
   digitalWrite(TRIG_D, LOW);
 
-  pinMode(MOTOR_E, OUTPUT);
-  pinMode(MOTOR_D, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
   Serial.begin(9600);
 
 }
 
 float distance_detection(int trig, int echo) //funcao pra identificar as distancias a partir de cada ultrassonico
 {
+  digitalWrite(trig, LOW);
+  delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
@@ -52,31 +65,76 @@ void loop()
   Serial.println(esquerda_distancia);
   float direita_distancia = distance_detection(TRIG_D, ECHO_D);
   Serial.println(direita_distancia);
-  
- 
+
+  //condicões de movimento (logica da mão direita)
+
+  if (direita_distancia > DIST_P) //se a direita estiver livre, gire pra direita
+  {
+    parar();
+    delay(200);
+    girar_direita();
+    delay(500);
+  }
+  else if (frente_distancia > DIST_P) //se a direita estiver bloqueada, e a frente livre, mova pra frente
+  {
+    mover_frente();
+  }
+
+  else if (esquerda_distancia > DIST_P) //se a frente e a direita estiverem bloqueadas, mova pra esquerda
+  {
+    parar();
+    delay(200);
+    girar_esquerda();
+    delay(500);
+  }
+  else 
+  {
+    parar();
+    delay(200);
+    mover_tras();
+    delay(500);
+  }
+
 }
 
-//ajustar os movimentos conforme a PONTE H L298N
+//movimentos ajustados com Ponte H L298
 void mover_frente() 
 {
-  digitalWrite(MOTOR_E, HIGH);
-  digitalWrite(MOTOR_D, HIGH);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void mover_tras()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+
 }
 
 void parar()
 {
-  digitalWrite(MOTOR_E, LOW);
-  digitalWrite(MOTOR_D, LOW);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
 }
 
 void girar_esquerda()
 {
-  digitalWrite(MOTOR_E, HIGH);
-  digitalWrite(MOTOR_D, LOW);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
 }
 
 void girar_direita()
 {
-  digitalWrite(MOTOR_E, LOW);
-  digitalWrite(MOTOR_D, HIGH);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
 }
